612ca83e4de16124460cce4d6af1ece2f6ea0c67
Alex <alex.scherer@outlook.com>
2013-12-18 20:47:36 +0100

    finish cakephp init, fixes #1

--- app/Config/core.php
+++ app/Config/core.php
@@ -222,12 +222,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'fdjksaj32f32j0jfJ2F43KLggFgJf22fDSIfud9fdsajf9');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '5743827548926906320054205742578233895');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
